a8c1518be56b3fd83e9119a69c3a96b2874eba12
Ahmed Abdallah <ahmadpx@hotmail.com>
2018-11-02 11:44:15 +0400

    Don't extract css

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  devServer: {
-    proxy: "https://www-dev.tajawal.com/",
-    https: true,
-    port: 5000,
-    host: "www-dev.tajawal.local"
+  css: {
+    extract: false
   }
 };
